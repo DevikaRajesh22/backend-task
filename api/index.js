18bc1ab23237feb38508b407fb846fa4419741d5
DevikaRajesh22 <devikaraj699@gmail.com>
2024-07-25 21:46:25 +0530

    buf fixes

--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
