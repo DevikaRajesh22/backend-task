675733a9ea24ee763fc62e785e319e9c4001f4ba
DevikaRajesh22 <devikaraj699@gmail.com>
2024-07-25 21:58:49 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,42 +1,58 @@
 const express = require('express');
+const http = require('http');
 const socketIo = require('socket.io');
-const connectDB = require('../config/db');
-const productRoutes = require('../routes/productRoutes');
-const userRoutes = require('../routes/userRoutes');
-const weatherRoutes = require('../routes/weatherRoutes');
-const { notFound, errorHandler } = require('../middleware/errorHandler');
-const logger = require('../utils/logger');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const weatherRoutes = require('./routes/weatherRoutes');
+const { notFound, errorHandler } = require('./middleware/errorHandler');
+const logger = require('./utils/logger');
 
 // Load environment variables
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // Connect to MongoDB
 connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  console.log('Request Body:', req.body);
-  next();
+    console.log(`${req.method} ${req.url}`);
+    console.log('Request Body:', req.body);
+    next();
 });
 
 // Routes
+app.get('/api', (req, res) => {
+    res.send('Hello from Express!');
+});
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/weather', weatherRoutes);
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+    socket.on('newMessage', (message) => {
+        io.emit('messageReceived', message);
+    });
+});
 
 // Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-module.exports = (req, res) => {
-  return new Promise((resolve, reject) => {
-    app(req, res);
-    resolve();
-  });
-};
+// Start server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
