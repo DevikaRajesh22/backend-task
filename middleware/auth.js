6d22f5bf1ef957c37af22da42d0849a584155270
DevikaRajesh22 <devikaraj699@gmail.com>
2024-07-26 14:30:45 +0530

    fixed

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,25 +3,16 @@ const User = require('../models/user');
 require('dotenv').config();
 
 exports.protect = async (req, res, next) => {
-    const secret = process.env.JWT_SECRET;
-    let token;
-    console.log(req.headers.authorization)
+    let token
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        console.log('true')
         token = req.headers.authorization.split(' ')[1];
-        console.log('tok',token)
     }
-    console.log('token',token)
     if (!token) {
         return res.status(401).json({ message: 'Not authorized' });
     }
     try {
-        console.log('try')
-        console.log("sec",process.env.JWT_SECRET)
         const decoded = await jwt.verify(token, process.env.JWT_SECRET)
-        console.log('dec',decoded)
-        req.user = await User.findById(decoded.id).select('-password');
-        console.log('user',req.user)
+        req.user = await User.findById(decoded.id).select('-password')
         next();
     } catch (error) {
         res.status(401).json({ message: 'Not authorized' });
