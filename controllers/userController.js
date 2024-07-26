2064107b910ce2d4978e455b412c255b43a89b46
DevikaRajesh22 <devikaraj699@gmail.com>
2024-07-26 12:15:22 +0530

    consoles

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,7 @@ const registerUser = asyncHandler(async(req,res)=>{
         await user.save();
         res.status(201).json({ message: 'User registered' });
     } catch (error) {
+        console.error('Error occurred:', error);
         res.status(500).json({ message: 'Server error' });
     }
 });
