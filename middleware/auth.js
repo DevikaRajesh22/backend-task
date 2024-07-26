04716f08ed7be05859e184ede74d814cd09c9a58
DevikaRajesh22 <devikaraj699@gmail.com>
2024-07-26 14:00:41 +0530

    authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,7 +18,7 @@ exports.protect = async (req, res, next) => {
     try {
         console.log('try')
         console.log("sec",secret)
-        const decoded = jwt.verify(token, secret);
+        const decoded = await jwt.verify(token, secret);
         console.log('dec',decoded)
         req.user = await User.findById(decoded.id).select('-password');
         console.log('user',req.user)
